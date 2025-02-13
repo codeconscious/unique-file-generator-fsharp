e33ff8d3f5f39030e7441048bf8990e51b70e89c
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-02-13 22:57:40 +0900

    Initial extension arg processing

--- src/UniqueFileGenerator/UniqueFileGenerator.Console/Program.fs
+++ src/UniqueFileGenerator/UniqueFileGenerator.Console/Program.fs
@@ -12,7 +12,23 @@ module Printing =
         | None -> printfn $"%s{msg}"
 
 module Main =
+    open ArgValidation
     open Printing
+
+    // TODO: Move elsewhere.
+    let private prepend args =
+        match args.Flags |> Map.tryFindKey (fun x _ -> x = "-p") with
+        | Some key ->
+            let text = args.Flags |> Map.find key
+            fun fileName -> $"%s{text}%s{fileName}"
+        | None -> id
+
+    let private appendExt args =
+        match args.Flags |> Map.tryFindKey (fun x _ -> x = "-e") with
+        | Some key ->
+            let ext = args.Flags |> Map.find key
+            fun fileName -> $"%s{fileName}.%s{ext}"
+        | None -> id
 
     [<EntryPoint>]
     let main args =
@@ -21,15 +37,24 @@ module Main =
 
         match validatedArgs with
         | Ok a ->
+            let ext = appendExt a
+            let pre = prepend a
+            let combined = ext >> pre
+
             StringGenerator.generateMultiple 128 a.FileCount
                 |> Array.iter (fun x ->
-                    x |> printColor (Some ConsoleColor.Cyan))
+                    x
+                    |> combined
+                    |> printColor (Some ConsoleColor.Cyan))
             StringGenerator.generateGuids a.FileCount
                 |> Array.iter (fun x ->
-                    x |> printColor (Some ConsoleColor.Blue))
+                    x
+                    |> combined
+                    |> printColor (Some ConsoleColor.Blue))
             $"Done after %s{watch.ElapsedFriendly}" |> printColor None
             0
         | Error e ->
             e |> printColor (Some ConsoleColor.Red)
             Help.printInstructions() |> ignore
             1
+
