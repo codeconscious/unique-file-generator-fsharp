8c4670a78c6b087c6a0e4ed2a644fb5ddc07d332
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-02-14 17:47:12 +0900

    Arg validation refactor

--- src/UniqueFileGenerator/UniqueFileGenerator.Console/ArgParser.fs
+++ src/UniqueFileGenerator/UniqueFileGenerator.Console/ArgParser.fs
@@ -3,16 +3,29 @@ namespace UniqueFileGenerator.Console
 open System
 
 module ArgValidation =
-    type ParsedArgs = { FileCount: int; Flags: Map<string, string> }
+    type Options =
+        { Prefix: string option
+          Extension: string option
+          OutputDirectory: string option
+          Size: int option
+          Delay: int option }
 
-    let private supportedFlags =
-        [
-            "-p"; // Prefix
-            "-e"; // Extension
-            "-o"; // Output directory
-            "-s"; // Size
-            "-d"  // Delay
-        ]
+    type Args =
+        { FileCount: int
+          Options: Options }
+
+    let prefixFlag = "-p"
+    let extensionFlag = "-e"
+    let outputDirectoryFlag = "-o"
+    let sizeFlag = "-s"
+    let delayFlag = "-d"
+
+    let supportedFlags =
+        [ prefixFlag
+          extensionFlag
+          outputDirectoryFlag
+          sizeFlag
+          delayFlag ]
 
     type ResultBuilder() =
         member this.Bind(m, f) =
@@ -25,48 +38,76 @@ module ArgValidation =
 
     let result = ResultBuilder()
 
-    let private validate args =
-        let hasMalformedFlag (flags: string seq) =
-            not(flags |> Seq.forall (fun f -> f[0] = '-'))
-        let hasUnsupportedFlag (flags: string seq) =
-            flags |> Seq.exists (fun f -> not(supportedFlags |> List.contains f))
+    let private tryParseInt (input: string) =
+        match Int32.TryParse(input.Replace(", ", String.Empty)) with
+        | true, i -> Some i
+        | false, _ -> None
 
-        match args with
-        | a when a.FileCount < 1 ->
-            Error "File count must be one or greater."
-        | a when a.Flags.Keys |> hasMalformedFlag ->
-            Error "Malformed flag(s) found. Each flag must start with a single hyphen (\"-\")."
-        | a when a.Flags.Keys |> hasUnsupportedFlag ->
-            Error "Unsupported flag(s) found."
+    let private verifyArgCount (args: string array) =
+        let isEven i = i % 2 = 0
+
+        match args.Length with
+        | 0 -> Error "You must pass in at least one argument: the number of files to generate."
+        | l when isEven l -> Error "Invalid argument count."
         | _ -> Ok args
 
-    let private isEven i =
-        i % 2 = 0
+    let private verifyFileCount (args: string array) =
+        let countArg = args |> Array.head
+        match tryParseInt countArg with
+        | None -> Error $"Invalid file count: %s{countArg}."
+        | Some c -> Ok c
 
-    let private toPairs arr =
-        arr
-        |> Array.chunkBySize 2
-        |> Array.map (fun x -> (x[0], x[1]))
+    let private parseOptions (options: string array) =
+        let hasMalformedOption (optionPairs: string seq) =
+            let isCorrectFormat (o: string) =
+                o.Length = 2 &&
+                o.StartsWith("-") &&
+                Char.IsLetter o[1]
 
-    let private tryParseNumber (input: string) =
-        match Int32.TryParse(input.Replace(", ", String.Empty)) with
-        | true, value -> Ok value
-        | false, _ -> Error "File count must be one or greater."
+            optionPairs
+            |> Seq.forall isCorrectFormat
+            |> not
 
-    let startValidation (args: string array) =
-        result {
-            let! args' =
-                match args.Length with
-                | 0 -> Error "You must pass in at least one argument."
-                | l when isEven l -> Error "Invalid argument count!"
-                | _ -> Ok args
+        let extractValue option map =
+            map
+            |> Map.tryFind option
 
-            let! count = args' |> Array.head |> tryParseNumber
-            let args'' = {
-                FileCount = count
-                Flags = args' |> Array.tail |> toPairs |> Map.ofArray // TODO: Should break if dupes.
+        let hasUnsupportedOption (options: string seq) =
+            let isUnsupported (o: string) =
+                supportedFlags
+                |> List.contains o
+                |> not
+
+            options
+            |> Seq.exists isUnsupported
+
+        let optionMap =
+            options
+            |> Array.tail // Ignore the file count.
+            |> Array.chunkBySize 2 // Will throw if array length is odd!
+            |> Array.map (fun x -> (x[0], x[1]))
+            |> Map.ofArray
+
+        match optionMap with
+        | o when o.Keys |> hasMalformedOption ->
+            Error "Malformed flag(s) found."
+        | o when o.Keys |> hasUnsupportedOption ->
+            Error "Unsupported flag(s) found."
+        | o ->
+            Ok {
+                Prefix =          o |> extractValue prefixFlag
+                Extension =       o |> extractValue extensionFlag
+                OutputDirectory = o |> extractValue outputDirectoryFlag
+                Size =            o |> extractValue sizeFlag
+                                    |> Option.bind tryParseInt
+                Delay =           o |> extractValue delayFlag
+                                    |> Option.bind tryParseInt
             }
 
-            let! args''' = validate args''
-            return args'''
+    let validate (args: string array) =
+        result {
+            let! args' = verifyArgCount args
+            let! fileCount = verifyFileCount args'
+            let! options = parseOptions args'
+            return { FileCount = fileCount; Options = options }
         }
--- src/UniqueFileGenerator/UniqueFileGenerator.Console/Program.fs
+++ src/UniqueFileGenerator/UniqueFileGenerator.Console/Program.fs
@@ -11,45 +11,44 @@ module Printing =
             Console.ResetColor()
         | None -> printfn $"%s{msg}"
 
+// module IO =
+
+
 module Main =
     open ArgValidation
     open Printing
 
     // TODO: Move elsewhere.
     let private prepend args =
-        match args.Flags |> Map.tryFindKey (fun x _ -> x = "-p") with
-        | Some key ->
-            let text = args.Flags |> Map.find key
-            fun fileName -> $"%s{text}%s{fileName}"
+        match args.Options.Prefix with
+        | Some p -> fun fileName -> $"%s{p}%s{fileName}"
         | None -> id
 
     let private appendExt args =
-        match args.Flags |> Map.tryFindKey (fun x _ -> x = "-e") with
-        | Some key ->
-            let ext = args.Flags |> Map.find key
-            fun fileName -> $"%s{fileName}.%s{ext}"
+        match args.Options.Extension with
+        | Some e -> fun fileName -> $"%s{fileName}.%s{e}"
         | None -> id
 
     [<EntryPoint>]
     let main args =
         let watch = Startwatch.Library.Watch()
-        let validatedArgs = ArgValidation.startValidation args
+        let validatedArgs = ArgValidation.validate args
 
         match validatedArgs with
         | Ok a ->
             let ext = appendExt a
             let pre = prepend a
-            let combined = ext >> pre
+            let textProcessing = ext >> pre
 
             StringGenerator.generateMultiple 128 a.FileCount
                 |> Array.iter (fun x ->
                     x
-                    |> combined
+                    |> textProcessing
                     |> printColor (Some ConsoleColor.Cyan))
             StringGenerator.generateGuids a.FileCount
                 |> Array.iter (fun x ->
                     x
-                    |> combined
+                    |> textProcessing
                     |> printColor (Some ConsoleColor.Blue))
             $"Done after %s{watch.ElapsedFriendly}" |> printColor None
             0
