646dbf263b82651d29091523554dd6a7688ae44e
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-02-24 21:28:14 +0900

    Add success cases

--- src/UniqueFileGenerator.Tests/ArgParserTests.fs
+++ src/UniqueFileGenerator.Tests/ArgParserTests.fs
@@ -4,6 +4,13 @@ open UniqueFileGenerator.Console.ArgValidation
 open UniqueFileGenerator.Console.ArgValidation.Types
 open Xunit
 
+let prefix = "PREFIX_"
+let extension = ".txt"
+let baseLength = 60
+let directory = "何らかのフォルダー名"
+let size = 1_000_000
+let delay = 5_000
+
 [<Fact>]
 let ``Appropriate error when no args`` () =
     let emptyArgs = [||]
@@ -74,7 +81,104 @@ let ``Success when valid file count`` () =
     let args = [| "1000"; |]
     let actual = validate args
     let expected = Ok { FileCount = 1000; Options = defaultOptions }
-    // printfn "%A" actual
-    // printfn "%A" expected
     Assert.Equal(actual, expected)
 
+[<Fact>]
+let ``Success when valid file count with prefix`` () =
+    let args = [| "1000"; "-p"; prefix |]
+    let actual = validate args
+    let expected = Ok {
+        FileCount = 1000
+        Options = { defaultOptions with Prefix = prefix }
+    }
+    Assert.Equal(actual, expected)
+
+[<Fact>]
+let ``Success when valid file count with prefix and extension`` () =
+    let args = [| "1000"; "-p"; prefix; "-e"; extension |]
+    let actual = validate args
+    let expected = Ok {
+        FileCount = 1000
+        Options = { defaultOptions with Prefix = prefix; Extension = extension[1..] }
+    }
+    Assert.Equal(actual, expected)
+
+[<Fact>]
+let ``Success when valid file count with prefix, extension, and base length`` () =
+    let args = [|
+        "1000"
+        "-p"; prefix
+        "-e"; extension
+        "-b"; string baseLength
+    |]
+
+    let actual = validate args
+    let expected = Ok {
+        FileCount = 1000
+        Options = { defaultOptions with
+                        Prefix = prefix
+                        Extension = extension[1..]
+                        NameBaseLength = baseLength }
+    }
+    Assert.Equal(actual, expected)
+
+[<Fact>]
+let ``Success when valid file count with prefix, extension, base length, and custom subdirectory`` () =
+    let args = [|
+        "1000"
+        "-p"; prefix
+        "-e"; extension
+        "-b"; string baseLength
+        "-o"; directory
+    |]
+
+    let actual = validate args
+    let expected = Ok {
+        FileCount = 1000
+        Options = { defaultOptions with
+                        Prefix = prefix
+                        Extension = extension[1..]
+                        NameBaseLength = baseLength
+                        OutputDirectory = directory }
+    }
+    Assert.Equal(actual, expected)
+
+[<Fact>]
+let ``Success when valid file count with prefix, extension, base length, custom subdirectory, and size`` () =
+    let args = [| "1000"; "-p"; prefix; "-e"; extension; "-b"; string baseLength; "-o"; directory; "-s"; string size |]
+    let actual = validate args
+    let expected = Ok {
+        FileCount = 1000
+        Options = { defaultOptions with
+                        Prefix = prefix
+                        Extension = extension[1..]
+                        NameBaseLength = baseLength
+                        OutputDirectory = directory
+                        Size = Some size }
+    }
+    Assert.Equal(actual, expected)
+
+[<Fact>]
+let ``Success when valid file count with prefix, extension, base length, custom subdirectory, size, and delay`` () =
+    let args = [|
+        "1000"
+        "-p"; prefix
+        "-e"; extension
+        "-b"; string baseLength
+        "-o"; directory
+        "-s"; string size
+        "-d"; string delay
+    |]
+
+    let actual = validate args
+    let expected = Ok {
+        FileCount = 1000
+        Options = { defaultOptions with
+                        Prefix = prefix
+                        Extension = extension[1..]
+                        NameBaseLength = baseLength
+                        OutputDirectory = directory
+                        Size = Some size
+                        Delay = delay }
+    }
+    Assert.Equal(actual, expected)
