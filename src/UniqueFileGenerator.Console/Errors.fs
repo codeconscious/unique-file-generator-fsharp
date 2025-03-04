445935c8658ce6c05e88132f272d6a6138345f3c
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-03-04 22:46:27 +0900

    Add new error

--- src/UniqueFileGenerator.Console/Errors.fs
+++ src/UniqueFileGenerator.Console/Errors.fs
@@ -1,12 +1,20 @@
 namespace UniqueFileGenerator.Console
 
+open System
+open System.Globalization
+
 module Errors =
+
+    let private formatInt (i: int) =
+        i.ToString("N0", CultureInfo.InvariantCulture)
+
     type ErrorType =
         | NoArgsPassed
         | ArgCountInvalid
         | FileCountInvalid of string
         | MalformedFlags
         | UnsupportedFlags
+        | UnparsableToPositiveInt of string
         | DirectoryMissing of string
         | DriveSpaceConfirmationFailure
         | DriveSpaceInsufficient of Needed: string * Actual: string
@@ -19,6 +27,7 @@ module Errors =
         | FileCountInvalid c -> $"Invalid file count: %s{c}."
         | MalformedFlags -> "Malformed flag(s) found."
         | UnsupportedFlags -> "Unsupported flag(s) found."
+        | UnparsableToPositiveInt x -> $"Could not parse \"%s{x}\" to an integer between 1 and %s{formatInt Int32.MaxValue}."
         | DirectoryMissing e -> $"Directory \"%s{e}\" was not found."
         | DriveSpaceConfirmationFailure -> "Could not confirm available drive space."
         | DriveSpaceInsufficient (needed, actual) ->
