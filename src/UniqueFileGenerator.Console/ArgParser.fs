402f9928dd48c83d244749da86ee53dbfdca5b3f
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-03-06 17:47:27 +0900

    Refactor numeric argument parsing (#12)
    * Parse name base length
    
    * Parse size arg
    
    * Parse delay arg
    
    * Simplify validation
    
    * Minor tweaks
    
    * Minor cleanup

--- src/UniqueFileGenerator.Console/ArgParser.fs
+++ src/UniqueFileGenerator.Console/ArgParser.fs
@@ -26,38 +26,51 @@ module ArgValidation =
             { FileCount: int
               Options: Options }
 
+        type RawOptionPairs = Map<string,string>
+
         let flags: Map<OptionType, string> =
-            [ (Prefix, "-p")
-              (NameBaseLength, "-b")
-              (Extension, "-e")
-              (OutputDirectory, "-o")
-              (Size, "-s")
-              (Delay, "-d") ]
+            [ Prefix, "-p"
+              NameBaseLength, "-b"
+              Extension, "-e"
+              OutputDirectory, "-o"
+              Size, "-s"
+              Delay, "-d" ]
             |> Map.ofList
 
     open Types
 
-    let private empty = String.Empty
-
     let defaultOptions =
-        { Prefix = empty
+        { Prefix = String.Empty
           NameBaseLength = 50
-          Extension = empty
+          Extension = String.Empty
           OutputDirectory = "output"
           Size = None
           Delay = 0 }
 
-    let supportedSeparators = [ ","; "_" ]
 
-    let stripSeparators separators text =
-        separators
-        |> List.fold (fun (acc: string) s -> acc.Replace(s, empty)) text
+    let stripSeparators text =
+        let supportedSeparators = [ ","; "_" ]
+
+        supportedSeparators
+        |> List.fold (fun (acc: string) s ->
+            acc.Replace(s, String.Empty)) text
 
     let private tryParseInt (input: string) =
-        let strippedArg = input |> stripSeparators supportedSeparators
+        let strippedArg = input |> stripSeparators
+
         match Int32.TryParse strippedArg with
         | true, i -> Some i
         | false, _ -> None
+
+    let private parseInRange (floor, ceiling) (x: string) =
+        let error = InvalidNumber (x, floor, ceiling)
+
+        match tryParseInt x with
+        | None -> Error error
+        | Some i ->
+            if i < floor then Error error
+            elif i > ceiling then Error error
+            else Ok i
 
     let private verifyArgCount (args: string array) =
         let isEven i = i % 2 = 0
@@ -65,22 +78,48 @@ module ArgValidation =
         match args.Length with
         | 0 -> Error NoArgsPassed
         | l when isEven l -> Error ArgCountInvalid
-        | _ -> Ok args
+        | _ -> Ok ()
 
-    let private verifyFileCount (args: string array) =
-        let rawArg = Array.head args
-        let strippedArg = rawArg |> stripSeparators supportedSeparators
+    let private verifyFileCount arg =
+        let strippedArg = arg |> stripSeparators
 
         match tryParseInt strippedArg with
         | Some c ->
             if c > 0 then Ok c
-            else Error (FileCountInvalid rawArg)
-        | None -> Error (FileCountInvalid rawArg)
+            else Error (FileCountInvalid arg)
+        | None -> Error (FileCountInvalid arg)
 
-    let private parseOptions options =
-        let ensureBetween (floor, ceiling) i =
-            i |> max floor |> min ceiling
+    let private toOptionPairs argPairs =
+        argPairs
+        |> Array.chunkBySize 2 // Will throw if array length is odd!
+        |> Array.map (fun x -> x[0], x[1])
+        |> Map.ofArray
 
+    let private parseBaseLength optionPairs =
+        optionPairs
+        |> Map.tryFind flags[NameBaseLength]
+        |> function
+            | None -> Ok defaultOptions.NameBaseLength
+            | Some x -> x |> parseInRange (1, Int32.MaxValue)
+
+    let private parseSize optionPairs =
+        optionPairs
+        |> Map.tryFind flags[Size]
+        |> function
+            | None -> Ok None
+            | Some x ->
+                match (x |> parseInRange (1, Int32.MaxValue)) with
+                | Error e -> Error e
+                | Ok i -> Ok (Some i)
+
+    let private parseDelay optionPairs =
+        optionPairs
+        |> Map.tryFind flags[Delay]
+        |> function
+            | None -> Ok defaultOptions.Delay
+            | Some x -> x |> parseInRange (0, Int32.MaxValue)
+
+    let private verifyFlags (optionPairs: RawOptionPairs) =
         let hasMalformedOption optionPairs =
             let isCorrectFormat (o: string) =
                 o.Length = 2 &&
@@ -90,11 +129,6 @@ module ArgValidation =
             optionPairs
             |> Seq.forall isCorrectFormat
             |> not
-
-        let extractValue option fallback map =
-            map
-            |> Map.tryFind option
-            |> Option.defaultValue fallback
 
         let hasUnsupportedOption options =
             let isUnsupported option =
@@ -106,38 +140,40 @@ module ArgValidation =
             options
             |> Seq.exists isUnsupported
 
-        let optionMap =
-            options
-            |> Array.tail // Disregard the file count.
-            |> Array.chunkBySize 2 // Will throw if array length is odd!
-            |> Array.map (fun x -> x[0], x[1])
-            |> Map.ofArray
+        match optionPairs with
+        | o when o.Keys |> hasMalformedOption -> Error MalformedFlags
+        | o when o.Keys |> hasUnsupportedOption -> Error UnsupportedFlags
+        | _ -> Ok ()
 
-        match optionMap with
-        | o when o.Keys |> hasMalformedOption ->
-            Error MalformedFlags
-        | o when o.Keys |> hasUnsupportedOption ->
-            Error UnsupportedFlags
-        | o ->
-            Ok {
-                Prefix =          o |> extractValue flags[Prefix] defaultOptions.Prefix
-                NameBaseLength =  o |> extractValue flags[NameBaseLength] empty
-                                    |> tryParseInt
-                                    |> Option.defaultValue defaultOptions.NameBaseLength
-                                    |> ensureBetween (1, 100)
-                Extension =       o |> extractValue flags[Extension] defaultOptions.Extension
-                OutputDirectory = o |> extractValue flags[OutputDirectory] defaultOptions.OutputDirectory
-                Size =            o |> extractValue flags[Size] empty
-                                    |> tryParseInt
-                Delay =           o |> extractValue flags[Delay] empty
-                                    |> tryParseInt
-                                    |> Option.defaultValue defaultOptions.Delay
-            }
+    let private toOptions (optionMap : RawOptionPairs) baseLength size delay =
+        let extractValue option fallback map =
+            map
+            |> Map.tryFind option
+            |> Option.defaultValue fallback
 
-    let validate (args: string array) =
+        Ok {
+            Prefix          = optionMap |> extractValue flags[Prefix] defaultOptions.Prefix
+            NameBaseLength  = baseLength
+            Extension       = optionMap |> extractValue flags[Extension] defaultOptions.Extension
+            OutputDirectory = optionMap |> extractValue flags[OutputDirectory] defaultOptions.OutputDirectory
+            Size            = size
+            Delay           = delay
+        }
+
+    let validate args =
         result {
-            let! args' = verifyArgCount args
-            let! fileCount = verifyFileCount args'
-            let! options = parseOptions args'
-            return { FileCount = fileCount; Options = options }
+            let! _ = verifyArgCount args
+            let fileCountArg, optionArgs = args[0], args[1..]
+
+            let! fileCount = verifyFileCount fileCountArg
+
+            let optionPairs = optionArgs |> toOptionPairs
+            let! _ = verifyFlags optionPairs
+            let! b = parseBaseLength optionPairs
+            let! s = parseSize optionPairs
+            let! d = parseDelay optionPairs
+            let! options = toOptions optionPairs b s d
+
+            return { FileCount = fileCount
+                     Options = options }
         }
--- src/UniqueFileGenerator.Console/Errors.fs
+++ src/UniqueFileGenerator.Console/Errors.fs
@@ -14,6 +14,7 @@ module Errors =
         | FileCountInvalid of string
         | MalformedFlags
         | UnsupportedFlags
+        | InvalidNumber of string * int * int
         | DirectoryMissing of string
         | DriveSpaceConfirmationFailure
         | DriveSpaceInsufficient of Needed: string * Actual: string
@@ -26,6 +27,7 @@ module Errors =
         | FileCountInvalid c -> $"Invalid file count: %s{c}."
         | MalformedFlags -> "Malformed flag(s) found."
         | UnsupportedFlags -> "Unsupported flag(s) found."
+        | InvalidNumber (x, f, c) -> $"Could not parse \"%s{x}\" to an integer between %s{formatInt f} and %s{formatInt c}."
         | DirectoryMissing e -> $"Directory \"%s{e}\" was not found."
         | DriveSpaceConfirmationFailure -> "Could not confirm available drive space."
         | DriveSpaceInsufficient (needed, actual) ->
