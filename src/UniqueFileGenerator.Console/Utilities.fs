35c0cd0e579d5bbff7318738e7630ca39a78af55
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-09-17 21:23:06 +0900

    Code layout

--- src/UniqueFileGenerator.Console/Utilities.fs
+++ src/UniqueFileGenerator.Console/Utilities.fs
@@ -20,14 +20,7 @@ module Utilities =
 
     // Numeric operations.
     type Num =
-
         static let culture = CultureInfo.InvariantCulture
-
-        static member Format(i: int) : string =
-            i.ToString("#,##0", culture)
-
-        static member Format(i: int64) : string =
-            i.ToString("#,##0", culture)
-
-        static member Format(f: float) : string =
-            f.ToString("#,##0.00", culture)
+        static member Format(i: int)   : string = i.ToString("#,##0", culture)
+        static member Format(i: int64) : string = i.ToString("#,##0", culture)
+        static member Format(f: float) : string = f.ToString("#,##0.00", culture)
